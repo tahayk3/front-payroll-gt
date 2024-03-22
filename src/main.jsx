443b30d9ec72ea43ee31b3d976134f5649cb6aca
JMizraim <jomizraim47@gmail.com>
2024-03-22 12:27:37 -0600

    Add notification service

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+//toastify
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
 // components
 import App from './components/App'
 
@@ -10,5 +14,6 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+    <ToastContainer />
+  </React.StrictMode>,
 )
