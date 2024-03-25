1e01f1442adffadb28953ffaceca7c22b927b0aa
ESMERALD22 <esmeraldvl@umes.edu.gt>
2024-03-25 14:25:26 -0600

    feat: Integración de métodos para manejo de local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,68 @@
-import {Button} from './ui/button'
-
+import { Button } from './ui/button'
+import '../index.css'
+import React, { useState } from 'react';
+import { useLocalStorage } from "../services/local-storage";
 
 const App = () => {
+  const [inputText, setInputText, removeItem] = useLocalStorage("nombreUsuario", '');
+  const [nombre, setNombre] = useState("");
+  const [nombreMostrar, setNombreMostra] = useState("");
+
+  const handleInputChange = (e) => {
+    const text = e.target.value
+    setNombre(text);
+    console.log(text);
+  }
+
+  const saveData = (event) => {
+    setInputText(nombre);
+    alert('Valor del input guardado en el almacenamiento local: ');
+  }
+  const setData = () => {
+    setNombreMostra(inputText);
+    alert("Se mostro el valor guardado");
+  };
+  const removeData = () => {
+    //Forma 1 de eliminar el item
+    //    setInputText("");
+    //forma 2 de eliminar item
+    removeItem();
+    setNombreMostra("");
+    alert("El valor del localStorage se ha eliminado");
+  };
+
   return (
     <div className='text-center text-2xl '>
-      <Button>Click</Button>
+      <label>
+        Ingresar nombre:
+        <br />
+        <input
+          type="text"
+          onChange={handleInputChange}
+          style={{ border: '1px solid black', padding: '5px', borderRadius: '5px' }}
+        />
+      </label>
+      <Button onClick={saveData} >Guardar</Button>
+
+
+      <br />
+      <br />
+      <br />
+      <label>
+        Valor guardado:
+        <br />
+        <input
+          type="text"
+          value={nombreMostrar}
+          readOnly
+          style={{ border: '1px solid black', padding: '5px', borderRadius: '5px' }}
+        />
+      </label>
+      <Button onClick={setData}>Recuperar valor del localStorage</Button>
+
+      <br />
+      <br />
+      <Button onClick={removeData}>Eliminar valor del localStorage</Button>
     </div>
   )
 }
