16ab0bbd44b063559b427e5a47b677518dcd3475
ESMERALD22 <esmeraldvl@umes.edu.gt>
2024-03-26 11:18:13 -0600

    feat: se agregó el servicio de session

---
+++ src/services/session-service.js
@@ -0,0 +1,35 @@
+
+class SessionService
+ {
+  constructor() { }
+
+  set(clave, valor) {
+    const valorString = JSON.stringify(valor);
+    sessionStorage.setItem(clave, valorString);
+    console.log('Valor guardado.  "' + valorString);
+  }
+
+  get(clave) {
+    const valorString = sessionStorage.getItem(clave);
+    const valorJSON = JSON.parse(valorString);
+    console.log('Valor recuperado de sessionStorage bajo la clave "' + clave + '"---- ' + valorJSON);
+    return valorJSON;
+  }
+
+  remove(clave) {
+    sessionStorage.removeItem(clave);
+    console.log('Se ha eliminado el valor de sessionStorage bajo la clave "' + clave + '"' + sessionStorage);
+    console.log('Nuevo estado de sessionStorage:', sessionStorage);
+
+  }
+  removeAll() {
+    Object.keys(sessionStorage).forEach(key => {
+      sessionStorage.removeItem(key);
+    });
+
+    console.log('Se han eliminado todas las claves de sessionStorage');
+    console.log('Nuevo estado de sessionStorage:', sessionStorage);
+  }
+
+}
+export default Object.freeze(new SessionService())
