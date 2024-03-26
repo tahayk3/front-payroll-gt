161518715a5a9e4f9c639e9625c909a69ee59382
ESMERALD22 <esmeraldvl@umes.edu.gt>
2024-03-26 10:00:13 -0600

    feat: corrección de session service

---
+++ src/services/session-service.js
@@ -0,0 +1,33 @@
+
+class SessionStorageHandler {
+  constructor() { }
+
+  set(clave, valor) {
+    const valorString = JSON.stringify(valor);
+    sessionStorage.setItem(clave, valorString);
+    console.log('Valor guardado.  "' + valorString);
+  }
+
+  get(clave) {
+    const valorString = sessionStorage.getItem(clave);
+    // Convertir la cadena de texto de vuelta a un objeto JSON
+    const valorJSON = JSON.parse(valorString);
+    console.log('Valor recuperado de sessionStorage bajo la clave "' + clave + '"---- ' + valorJSON);
+    return valorJSON;
+  }
+
+  remove(clave) {
+    sessionStorage.removeItem(clave);
+    console.log('Se ha eliminado el valor de sessionStorage bajo la clave "' + clave + '"' + sessionStorage);
+  }
+  removeAll() {
+    Object.keys(sessionStorage).forEach(key => {
+      sessionStorage.removeItem(key);
+    });
+
+    console.log('Se han eliminado todas las claves de sessionStorage');
+    console.log('Nuevo estado de sessionStorage:', sessionStorage);
+  }
+
+}
+export default SessionStorageHandler;
